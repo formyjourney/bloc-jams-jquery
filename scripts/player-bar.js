e48549c058e07310fd6305f7167b092e5fc31089
formyjourney <35152140+formyjourney@users.noreply.github.com>
2019-02-26 20:48:46 -0500

    checkpoint-player-sliders: completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -4,6 +4,17 @@
      $(this).attr('playState', player.playState);
    });
 
+   $('button#previous').on('click', function() {
+      if (player.playState !== 'playing') { return; }
+
+      const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+      const previousSongIndex = currentSongIndex - 1;
+      if (previousSongIndex < 0) { return; }
+
+      const previousSong = album.songs[previousSongIndex];
+      player.playPause(previousSong);
+    });
+
    $('button#next').on('click', function() {
      if (player.playState !== 'playing') { return; }
 
@@ -14,4 +25,17 @@
      const nextSong = album.songs[nextSongIndex];
      player.playPause(nextSong);
    });
- }
+
+$('#time-control input').on('input', function (event) {
+  player.skipTo(event.target.value);
+});
+
+setInterval( () => {
+  if (player.playState !== 'playing') { return; }
+  const currentTime = player.getTime();
+  const duration = player.getDuration();
+  const percent = (currentTime / duration) * 100;
+  $('#time-control .current-time').text( currentTime );
+  $('#time-control input').val(percent);
+}, 1000);
+}
